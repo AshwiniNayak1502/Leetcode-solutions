a43d905439e94ff8d5861ce56f1dada308845188
AshwiniNayak1502 <nayakashwini1502@gmail.com>
2025-03-14 14:49:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.7 MB (99.97%)

---
+++ 0367-valid-perfect-square/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long left=1;
+        long right=num;
+        while(left<=right)
+        {
+            long mid=left+(right-left)/2;
+            long s=mid*mid;
+            if(s==num)
+            {
+                return true;
+            }
+            else if(s>num)
+            {
+                right=mid-1;
+            }
+            else
+            {
+                left=mid+1;
+            }
+            
+        }
+        return false;
+    }
+}
