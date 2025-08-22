f6c0544ac1ab68e06298f4e783ded051bbee2209
AshwiniNayak1502 <nayakashwini1502@gmail.com>
2025-08-22 16:21:12 +0000

    Sync LeetCode submission Runtime - 1 ms (99.89%), Memory - 46.6 MB (99.90%)

---
+++ 0283-move-zeroes/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       
+        int j=0;
+        
+        for(int i:nums){
+            if(i!=0){
+                nums[j++]=i;
+            }
+        }
+        while(j<nums.length){
+            nums[j++]=0;
+        }
+        
+    }
+}
