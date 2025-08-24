907d6143409ae9154ab047cd96dbdbc219b2845b
AshwiniNayak1502 <nayakashwini1502@gmail.com>
2025-08-24 15:58:35 +0000

    Sync LeetCode submission Runtime - 1 ms (97.45%), Memory - 45.2 MB (99.92%)

---
+++ 0724-find-pivot-index/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int left=0,right=0,total=0;
+        for(int i:nums) total+=i;
+        for(int i=0;i<nums.length;i++){
+            right=total-left-nums[i];
+            if(left==right) return i;
+            left+=nums[i];
+        }
+        return -1;
+    }
+}
