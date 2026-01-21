2f18cb2acdc8b05ede653917ba32089c4b02eabe
AshwiniNayak1502 <nayakashwini1502@gmail.com>
2026-01-21 04:34:10 +0000

    Sync LeetCode submission Runtime - 540 ms (20.12%), Memory - 0.0B (100.00%)

---
+++ 0177-nth-highest-salary/solution.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+Set N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+        Select Distinct salary
+        From Employee
+        Order By salary Desc
+        Limit 1 Offset N
+  );
+END
