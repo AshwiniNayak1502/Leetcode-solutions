d687f53a0bd07a565cd8b4af7cede7981e740313
AshwiniNayak1502 <nayakashwini1502@gmail.com>
2026-07-05 10:02:43 +0000

    Sync LeetCode submission Runtime - 44 ms (80.69%), Memory - 130.5 MB (85.54%)

---
+++ 0146-lru-cache/solution.java
@@ -0,0 +1,76 @@
+class LRUCache {
+    class Node{
+        int key,val;
+        Node prev,next;
+
+        public Node(int key,int val){
+            this.key=key;
+            this.val=val;
+        }
+    }
+    private int capacity;
+    private HashMap<Integer,Node> map;
+    Node head,tail;
+
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+        map=new HashMap<>();
+
+        head=new Node(0,0);
+        tail=new Node(0,0);
+
+        head.next=tail;
+        tail.prev=head;
+    }
+
+    public void insert(Node node){
+        node.prev=tail.prev;
+        node.next=tail;
+
+        tail.prev.next=node;
+        tail.prev=node;
+    }
+
+    public  void remove(Node node){
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+    } 
+    
+    public int get(int key) {
+        if(!map.containsKey(key)){
+            return -1;
+        }
+        Node node=map.get(key);
+        remove(node);
+        insert(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+         if(map.containsKey(key)){
+            Node node=map.get(key);
+            node.val=value;
+            remove(node);
+            insert(node);
+        }
+        else{
+            if(map.size()==capacity){
+                Node lru=head.next;
+                int r=lru.key;
+                remove(lru);
+                map.remove(r);
+            }
+            Node node=new Node(key,value);
+            insert(node);
+            map.put(key,node);
+        }
+       
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
