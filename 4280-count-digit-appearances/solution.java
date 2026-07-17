41cc064bc09a69e571187f9da5b7b77904681780
AshwiniNayak1502 <nayakashwini1502@gmail.com>
2026-07-17 05:30:58 +0000

    Sync LeetCode submission Runtime - 4 ms (97.81%), Memory - 46.7 MB (95.30%)

---
+++ 4280-count-digit-appearances/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+        int count=0;
+        for(int num:nums){
+            while(num!=0){
+                int d=num%10;
+                num/=10;
+                if(d==digit)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
