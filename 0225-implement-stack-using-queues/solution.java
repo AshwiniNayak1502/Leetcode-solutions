e19f3f93b440508baa364f79adf73bb4fed25618
AshwiniNayak1502 <nayakashwini1502@gmail.com>
2026-07-20 10:40:37 +0000

    Sync LeetCode submission Runtime - 2 ms (25.84%), Memory - 43 MB (17.00%)

--- 0225-implement-stack-using-queues/solution.java
+++ 0225-implement-stack-using-queues/solution.java
@@ -1,9 +1,11 @@
 class MyStack {
-    Queue<Integer> q1;
-    Queue<Integer> q2;
+        Queue<Integer> q1;
+        Queue<Integer> q2;
+
     public MyStack() {
         q1=new LinkedList<>();
         q2=new LinkedList<>();
+
     }
     
     public void push(int x) {
@@ -11,13 +13,13 @@ class MyStack {
         while(!q1.isEmpty()){
             q2.offer(q1.poll());
         }
-        Queue<Integer> temp=q2;
-        q2=q1;
-        q1=temp;
+        Queue temp=q1;
+        q1=q2;
+        q2=temp;
     }
     
     public int pop() {
-       return q1.poll();
+        return q1.poll();
     }
     
     public int top() {
